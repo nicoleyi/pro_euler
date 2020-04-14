85a23facf03944309623dcb2546d45cc3e5356fb
Nicole Yi <nicoleyi@google.com>
2020-04-14 22:37:57 +1000

    1. fix row issue when getting diagonal right to left. \n 2. add some comments.

--- q11-largest-product-in-grid.py
+++ q11-largest-product-in-grid.py
@@ -12,47 +12,49 @@ grid = [ ]
 
 fo.close()
 
-# main logic
+max_round, product_row, product_col, product_right, product_left = 0,0,0,0,0
+max_ = 0
+
 def product(myList):
     # Multiply elements one by one
     result = 1
     for i in range (len(myList)):
          result = result*myList[i]
     return result
-temp = product(grid[0][0:4])
-#print (temp)
-#print (grid[0][0:4])
-max_round, product_row, product_col, product_right, product_left = 0,0,0,0,0
-max_ = 0
+
+# main logic
 for i in range (20):
-  print ("Now i is ", i)
   for j in range (20):
-    print ("Now j is ", j)
+    # Get horizonal
     if j+3 < 20: 
       product_row = product([grid[i][j],
 			     grid[i][j+1],
 			     grid[i][j+2],
 			     grid[i][j+3]])
+    # Get vertical 
     if i+3 < 20:
       product_col = product([grid[i][j],
 			     grid[i+1][j],
 			     grid[i+2][j],
-			     grid[i+3][j]])
+    			     grid[i+3][j]])
+
+    # Get diagonal left to right
     if i+3 < 20 and j+3 < 20:
       product_right = product([grid[i][j],
 			       grid[i+1][j+1],
 			       grid[i+2][j+2],
 			       grid[i+3][j+3]])
-    if i-3 >= 0 and j-3 >= 0:
+    
+    # Get diagonal right to left
+    if i+3 < 20 and j-3 >= 0:
       product_left = product([grid[i][j],
-			      grid[i-1][j-1],
-			      grid[i-2][j-2],
-			      grid[i-3][j-3]])
+			      grid[i+1][j-1],
+			      grid[i+2][j-2],
+			      grid[i+3][j-3]])
 
     max_round = max(product_row, product_col,
 		    product_right,product_left,
 		    max_round)
-    print ("Round max is {}".format(max_round))
   max_ = max(max_,max_round)
 
 
